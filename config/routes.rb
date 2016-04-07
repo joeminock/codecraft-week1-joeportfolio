c3adfaf3963506ea5c9cb072d68af35281af3049
Joe Minock <joe.minock@gmail.com>
2016-04-07 14:59:41 -0600

    fixed bad character in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get '/contact' => 'pages#contact_me'
 	get '/web' => 'pages#web'
 	get '/wood' => 'pages#wood'
-	get '/test' => 'pages#test'\
+	get '/test' => 'pages#test'
 	get '/video' => 'pages#video'
 	
   # The priority is based upon order of creation: first created -> highest priority.
